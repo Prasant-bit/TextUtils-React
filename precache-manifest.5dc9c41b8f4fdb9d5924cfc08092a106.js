08d404a910c2da1bc3e45bb63800b755b3247824
Prasant <prasantmagar1997@gmail.com>
2021-09-07 21:40:49 +0545

    Updates

---
+++ precache-manifest.5dc9c41b8f4fdb9d5924cfc08092a106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e158c1a85233d376d71",
+    "url": "/TextUtils-React/static/css/main.d515e32d.chunk.css"
+  },
+  {
+    "revision": "0e158c1a85233d376d71",
+    "url": "/TextUtils-React/static/js/main.b8fa3dff.chunk.js"
+  },
+  {
+    "revision": "981ae331a34ed8d1d759",
+    "url": "/TextUtils-React/static/js/runtime~main.280084ec.js"
+  },
+  {
+    "revision": "ee7f37ea120d5f770ab3",
+    "url": "/TextUtils-React/static/js/2.18feb4ae.chunk.js"
+  },
+  {
+    "revision": "889af979780b03642b7f",
+    "url": "/TextUtils-React/static/js/3.942ba83d.chunk.js"
+  },
+  {
+    "revision": "f5f49571f4ebe0833c7b7c69278def76",
+    "url": "/TextUtils-React/index.html"
+  }
+];
